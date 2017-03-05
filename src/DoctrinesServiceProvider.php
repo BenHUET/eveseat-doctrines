006feb0d86dcf58532220ab57012302a62ea6c58
HUET Benjamin <benjamin.huet.1992@gmail.com>
2017-03-05 17:57:39 +0100

    base commit

---
+++ src/DoctrinesServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Seat\Kassie\Doctrines;
+
+use Illuminate\Support\ServiceProvider;
+
+class DoctrinesServiceProvider extends ServiceProvider
+{
+	public function boot()
+	{
+
+	}
+}
