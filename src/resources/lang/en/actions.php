d886bf572e468a9827f044cffe9042db5e8c14ea
HUET Benjamin <benjamin.huet.1992@gmail.com>
2017-11-05 13:28:46 +0100

    correction to static syntax

--- src/resources/lang/en/actions.php
+++ src/resources/lang/en/actions.php
@@ -12,13 +12,13 @@ return [
 'delete' => [
 	'category' => [
 		'question' => 'Delete this category ?',
-		'confirm' => 'Yes, I want to delete this category.',
-		'cancel' => 'No, do not delete this category.'
+		'confirm' => 'Yes, I want to delete this category',
+		'cancel' => 'No, do not delete this category'
 	]
 ],
 'update' => [
 	'category' => [
-		'confirm' => 'Update this category.'
+		'confirm' => 'Update this category'
 	]
 ]
 
